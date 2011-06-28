86a7ef5a18d81789179d8d4dee0815061c467f25
Chanita Siridechkun Anuwong <aireanalistic@gmail.com>
2011-06-28 22:44:29 +0700

    Add source and setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='csrf_tools',
+      version='1.0',
+      description='CSRF tools',
+      author='Proteus Technologies',
+      author_email='team@proteus-tech.com',
+      url='http://proteus-tech.com',
+      packages=['csrf_tools',],
+     )
